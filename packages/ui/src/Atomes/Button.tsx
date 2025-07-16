1d5ba20feefda683415cfd49b2a5f6cccec07355
aboudev22 <aboubakarjay2@gmail.com>
2025-07-16 18:50:34 +0100

    chore: add framer-motion to dependencies

--- packages/ui/src/Atomes/Button.tsx
+++ packages/ui/src/Atomes/Button.tsx
@@ -1,3 +1,13 @@
+import { motion } from "framer-motion";
+import "./button.css";
+
 export const Button = ({ label }: { label: string }) => {
-  return <button>{label}</button>;
+  return (
+    <motion.button
+      whileTap={{ scale: 0.9 }}
+      className="p-2 text-xs font-bold bg-black rounded-md w-fit text-pink-500  cursor-pointer"
+    >
+      {label}
+    </motion.button>
+  );
 };
