9af221c4f69609467705bb1c958696519ec5dcdb
aboudev22 <aboubakarjay2@gmail.com>
2025-07-21 17:15:46 +0100

    feat(HeroText): stagger animation with framer-motion

---
+++ packages/ui/src/Atoms/HeroText/HeroText.stories.tsx
@@ -0,0 +1,12 @@
+import { HeroText } from "./HeroText";
+
+export default {
+  title: "Atoms/HeroText",
+  component: HeroText,
+};
+
+export const Primary = {
+  args: {
+    text: "We are a boutique cybersecurity company",
+  },
+};
---
+++ packages/ui/src/Atoms/HeroText/HeroText.tsx
@@ -0,0 +1,57 @@
+import clsx from "clsx";
+import { motion } from "framer-motion";
+import "./HeroText.css";
+
+interface HeroTextProps {
+  text: string;
+  className?: string;
+}
+
+const containerVariants = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.2,
+      ease: "linear" as const,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const childVariants = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      ease: "linear" as const,
+    },
+  },
+};
+
+export const HeroText = ({ text, className }: HeroTextProps) => {
+  return (
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className={clsx(
+        "flex justify-center items-center flex-wrap dark:text-white font-extrabold text-7xl text-center w-5xl gap-3",
+        className
+      )}
+    >
+      {text.split(" ").map((item, index) => (
+        <motion.p
+          variants={childVariants}
+          key={`${index}-${item}`}
+          className="inline"
+        >
+          {item}
+        </motion.p>
+      ))}
+    </motion.div>
+  );
+};
