75089198f818c20c0882a0bc8f6ec4d998da5646
aboudev22 <aboubakarjay2@gmail.com>
2025-07-21 18:26:18 +0100

    feat(LinesMotion): staggerChildren

--- packages/ui/src/Molecules/LinesMotion/LinesMotion.tsx
+++ packages/ui/src/Molecules/LinesMotion/LinesMotion.tsx
@@ -9,18 +9,43 @@ interface LineMotionProps {
 
 const rotateProps = [-15, -8, -5, -2, 2, 5, 8, 15];
 
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+      staggerChildren: 0.1,
+      ease: "linear" as const,
+    },
+  },
+};
+const childrenVariants = {
+  hidden: { y: -50, opacity: 0 },
+  visible: { y: 0, opacity: 1 },
+  transition: {
+    duration: 0.4,
+  },
+};
+
 export const LinesMotion = ({ className }: LineMotionProps) => {
   return (
-    <motion.div
+    <div
       className={clsx(
         "flex h-full w-full justify-between items-center",
         className
       )}
     >
       {[...Array(2)].map((_, i) => (
-        <motion.section className="flex flex-col justify-between">
+        <motion.section
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+          className="flex flex-col justify-between"
+        >
           {[...Array(8)].map((_, index) => (
             <motion.div
+              variants={childrenVariants}
               style={{
                 rotate:
                   i === 0
@@ -38,6 +63,6 @@ export const LinesMotion = ({ className }: LineMotionProps) => {
           ))}
         </motion.section>
       ))}
-    </motion.div>
+    </div>
   );
 };
